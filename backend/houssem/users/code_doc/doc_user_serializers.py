6f0e4dc11a1f0185ab5d003a83098855b0962b68
Houssem ADOUNI <houssemadouni11@gmail.com>
2020-04-07 21:44:19 +0100

    gestion des profils

--- backend/houssem/users/code_doc/doc_user_serializers.py
+++ backend/houssem/users/code_doc/doc_user_serializers.py
@@ -9,3 +9,13 @@ class Doc_token_post(serializers.Serializer):
 
 class Doc_token_post_response(serializers.Serializer):
     token = serializers.CharField(required=True, max_length=100, help_text="le token")
+
+
+class Doc_code_request(serializers.Serializer):
+    code = serializers.CharField(required=True, max_length=100, help_text="le code du profile")
+
+
+class Doc_profil_post(serializers.Serializer):
+    username = serializers.CharField(required=True, max_length=100, help_text="le nom d'utilisateur")
+    password = serializers.CharField(required=True, max_length=100, help_text="le mot de passe")
+    code = serializers.CharField(required=True, max_length=100, help_text="le code")
